1d3e5ff73c2f8eb305221e3c8c21ca79dc420639
taingo1212 <taingo.bkcntt@gmail.com>
2018-11-21 18:31:06 +0700

    version 5 ready

--- lv_version1/controltraffic.py
+++ lv_version1/controltraffic.py
@@ -56,6 +56,21 @@ class Application(Frame):
     list_vehicle_ltkntd = []
     list_vehicle_lg = []
     list_vehicle_tht = []
+
+    list_vehiclepass_ltkbd = []
+    list_vehiclepass_ltkntd = []
+    list_vehiclepass_lg = []
+    list_vehiclepass_tht = []
+
+    nVhccome_ltkbd = 0
+    nVhccome_ltkntd = 0
+    nVhccome_lg = 0
+    nVhccome_tht = 0
+
+    nVhcpass_ltkbd = 0
+    nVhcpass_ltkntd = 0
+    nVhcpass_lg = 0
+    nVhcpass_tht = 0
     
     def __init__(self, parent):
         Frame.__init__(self, parent)
@@ -85,22 +100,43 @@ class Application(Frame):
         print('running')
         httphost_get = 'http://'+self.HOST+':'+self.PORT+'/home/gettldata' 
         httphost_put = 'http://'+self.HOST+':'+self.PORT+'/home/puttldata' 
+        httphost_putdetails = 'http://'+self.HOST+':'+self.PORT+'/home/puttldatadetails' 
         step = 0
+        putdatadetail = False
         while traci.simulation.getMinExpectedNumber() > 0:                  
-            traci.simulationStep()  
-            
-            print("time ",traci.simulation.getTime())
-            if (int(traci.simulation.getTime())%300) == 0:   
-                print("Vehicle come 5 minute ago")             
+            traci.simulationStep()                     
+
+            if (int(traci.simulation.getTime())%181) == 0:
+                putdatadetail = False
+
+            if (int(traci.simulation.getTime())%180) == 0:   
+                print("Vehicle come 3 minute ago")             
                 print("lg come: ", len(self.list_vehicle_lg))
                 print("tht come: ", len(self.list_vehicle_tht))
                 print("ltkbd come: ", len(self.list_vehicle_ltkbd))
                 print("ltkntd come: ", len(self.list_vehicle_ltkntd))
-                # clear list after 5 minute
-                self.list_vehicle_lg = []
-                self.list_vehicle_tht = []
-                self.list_vehicle_ltkbd = []
-                self.list_vehicle_ltkntd = []
+                
+                print("ltkbd pass: ", len(self.list_vehiclepass_ltkbd))
+                print("ltkntd pass: ", len(self.list_vehiclepass_ltkntd))
+                print("lg pass: ", len(self.list_vehiclepass_lg))
+                print("tht pass: ", len(self.list_vehiclepass_tht))
+
+                print(putdatadetail)    
+                # clear list after 3 minute                                
+                if (self.connected == True and  putdatadetail == False):
+                    print("putdata")
+                    info = {'id':'ltk_lg','apprid1':'ltklg_bd', 'apprid2':'ltklg_ntd', 'apprid3':'ltklg_lg', 'apprid4':'ltklg_tht', 'vhccome1': len(self.list_vehicle_ltkbd), 'vhccome2': len(self.list_vehicle_ltkntd), 'vhccome3': len(self.list_vehicle_lg), 'vhccome4': len(self.list_vehicle_tht), 'vhcout1': len(self.list_vehiclepass_ltkbd), 'vhcout2': len(self.list_vehiclepass_ltkntd), 'vhcout3': len(self.list_vehiclepass_lg), 'vhcout4': len(self.list_vehiclepass_tht)}      
+                    res = requests.get(httphost_putdetails, params = info)
+                    print(res)
+                    self.list_vehicle_lg = []
+                    self.list_vehicle_tht = []
+                    self.list_vehicle_ltkbd = []
+                    self.list_vehicle_ltkntd = []
+                    self.list_vehiclepass_ltkbd = []
+                    self.list_vehiclepass_ltkntd = []
+                    self.list_vehiclepass_lg = []
+                    self.list_vehiclepass_tht = []
+                    putdatadetail = True
 
             self.countVehicleCome(traci.inductionloop.getIDList())                             
 
@@ -192,17 +228,25 @@ class Application(Frame):
         sys.stdout.flush() 
 
     def countVehicleCome(self, listInductionLoopId):
-        global list_vehicle_ltkbd, list_vehicle_ltkntd, list_vehicle_lg, list_vehicle_tht
+        global list_vehicle_ltkbd, list_vehicle_ltkntd, list_vehicle_lg, list_vehicle_tht, list_vehiclepass_ltkbd, list_vehiclepass_ltkntd, list_vehiclepass_lg, list_vehiclepass_tht
         nvhc_lg = []       
         nvhc_tht = []
         nvhc_ltkbd = []
-        nvhc_ltkntd = []    
+        nvhc_ltkntd = []  
+
+        nvhc_pass_lg = []       
+        nvhc_pass_tht = []
+        nvhc_pass_ltkbd = []
+        nvhc_pass_ltkntd = []  
+
         for inductionLoopId in listInductionLoopId:
+            #count vehicle come
             if 'lg_detector' in inductionLoopId:                  
                 nvhc_lg = traci.inductionloop.getLastStepVehicleIDs(inductionLoopId)               
                 for vehicleId in nvhc_lg:
                     if (self.list_vehicle_lg.count(vehicleId) == 0):
                         self.list_vehicle_lg.append(vehicleId)
+
             if 'tht_detector' in inductionLoopId:             
                 nvhc_tht = traci.inductionloop.getLastStepVehicleIDs(inductionLoopId)  
                 for vehicleId in nvhc_tht:
@@ -220,6 +264,34 @@ class Application(Frame):
                 for vehicleId in nvhc_ltkntd:
                     if (self.list_vehicle_ltkntd.count(vehicleId) == 0):
                         self.list_vehicle_ltkntd.append(vehicleId)
+
+            # count vehicle pass     
+            if 'ltkbd_pass_detector' in inductionLoopId:             
+                nvhc_pass_ltkbd = traci.inductionloop.getLastStepVehicleIDs(inductionLoopId)     
+                for vehicleId in nvhc_pass_ltkbd:
+                    if (self.list_vehiclepass_ltkbd.count(vehicleId) == 0):
+                        self.list_vehiclepass_ltkbd.append(vehicleId)
+            # print(inductionLoopId)                        
+            # print("ltkbd pass: ", len(self.list_vehiclepass_ltkbd))
+
+            if 'ltkntd_pass_detector' in inductionLoopId:             
+                nvhc_pass_ltkntd = traci.inductionloop.getLastStepVehicleIDs(inductionLoopId)     
+                for vehicleId in nvhc_pass_ltkntd:
+                    if (self.list_vehiclepass_ltkntd.count(vehicleId) == 0):
+                        self.list_vehiclepass_ltkntd.append(vehicleId)
+
+            if 'lg_pass_detector' in inductionLoopId:             
+                nvhc_pass_lg = traci.inductionloop.getLastStepVehicleIDs(inductionLoopId)     
+                for vehicleId in nvhc_pass_lg:
+                    if (self.list_vehiclepass_lg.count(vehicleId) == 0):
+                        self.list_vehiclepass_lg.append(vehicleId)
+
+            if 'tht_pass_detector' in inductionLoopId:             
+                nvhc_pass_tht = traci.inductionloop.getLastStepVehicleIDs(inductionLoopId)     
+                for vehicleId in nvhc_pass_tht:
+                    if (self.list_vehiclepass_tht.count(vehicleId) == 0):
+                        self.list_vehiclepass_tht.append(vehicleId)                                    
+
 
         # print("lg come: ", len(self.list_vehicle_lg))
         # print("tht come: ", len(self.list_vehicle_tht))
